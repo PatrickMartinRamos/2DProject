aacd8562303819b7480c81ea86e63025e4251b47
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-03-22 21:33:17 +0800

    -added jump/waling/idle animation

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovements : MonoBehaviour
 {
     public float speed = 15f;
@@ -10,12 +11,14 @@ public class PlayerMovements : MonoBehaviour
     public int maxjumps = 2;
     private int jumpsremaining = 0;
     private bool isFacingRight = true;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         jumpsremaining = maxjumps;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,9 +28,11 @@ public class PlayerMovements : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         // ADD ANIMATION HERE SCRIPT >>
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         if(Input.GetKeyDown(KeyCode.Space) && jumpsremaining > 0)
         {
+            animator.SetBool("Jump", true);
             // ADD ANIMATION HERE SCRIPT >>
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
             jumpsremaining--;
@@ -50,9 +55,11 @@ public class PlayerMovements : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D other)
     {
+
         
         if(other.gameObject.CompareTag("Ground"))
         {
+            animator.SetBool("Jump", false);
             jumpsremaining = maxjumps;
         }
     }
